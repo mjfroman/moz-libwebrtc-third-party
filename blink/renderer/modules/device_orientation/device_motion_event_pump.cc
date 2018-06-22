cad2a25c68381715cb0c9c5394328ec275b7a09f
Oksana Zhuravlova <oksamyt@chromium.org>
2018-06-22 16:47:04 +0000

    device_sensors in content -> device_orientation in blink
    This change moves Device*EventPump classes and appropriate unit tests to
    third_party/blink/renderer/modules/device_orientation. Their instances
    are now created in relevant Device*Dispatcher methods with a LocalFrame*
    passed to be used for Mojo interface creation.
    Some updates were required to satisfy blink dependencies limitations
    and for consistency:
    - base::RepeatingTimer was replaced by TaskRunnerTimer which has to be
    initialized with a base::SingleThreadTaskRunner, so the constructors
    were extended with a new parameter;
    - base::Time -> WTF::Time;
    - base::BindOnce -> WTF::Bind, base::Unretained -> WTF::Unretained;
    - device::mojom:: -> device::mojom::blink::;
    - in the unit tests, device::mojom::SensorProviderPtrInfo is wrapped
    into device::mojom::blink::SensorProviderPtr since FakeSensorProvider
    implements device::mojom::SensorProvider
    
    Bug: 850997
    Change-Id: I440b9379f50ab42fc50b9d92490c2615ef8c3cc9
    Reviewed-on: https://chromium-review.googlesource.com/1099492
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#569659}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 4540438b4c21f8b23aa2b8dc8c333d3876c4f356

---
+++ blink/renderer/modules/device_orientation/device_motion_event_pump.cc
@@ -0,0 +1,135 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cmath>
+
+#include "services/device/public/cpp/generic_sensor/motion_data.h"
+#include "services/device/public/mojom/sensor.mojom-blink.h"
+#include "services/service_manager/public/cpp/interface_provider.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/modules/device_orientation/device_motion_event_pump.h"
+#include "ui/gfx/geometry/angle_conversions.h"
+
+namespace blink {
+
+template class DeviceSensorEventPump<blink::WebDeviceMotionListener>;
+
+DeviceMotionEventPump::DeviceMotionEventPump(
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner)
+    : DeviceSensorEventPump<blink::WebDeviceMotionListener>(task_runner),
+      accelerometer_(this, device::mojom::blink::SensorType::ACCELEROMETER),
+      linear_acceleration_sensor_(
+          this,
+          device::mojom::blink::SensorType::LINEAR_ACCELERATION),
+      gyroscope_(this, device::mojom::blink::SensorType::GYROSCOPE) {}
+
+DeviceMotionEventPump::~DeviceMotionEventPump() {
+  StopIfObserving();
+}
+
+void DeviceMotionEventPump::SendStartMessage(LocalFrame* frame) {
+  if (!sensor_provider_) {
+    DCHECK(frame);
+
+    frame->GetInterfaceProvider().GetInterface(
+        mojo::MakeRequest(&sensor_provider_));
+    sensor_provider_.set_connection_error_handler(
+        WTF::Bind(&DeviceSensorEventPump::HandleSensorProviderError,
+                  WTF::Unretained(this)));
+  }
+
+  accelerometer_.Start(sensor_provider_.get());
+  linear_acceleration_sensor_.Start(sensor_provider_.get());
+  gyroscope_.Start(sensor_provider_.get());
+}
+
+void DeviceMotionEventPump::SendStopMessage() {
+  // SendStopMessage() gets called both when the page visibility changes and if
+  // all device motion event listeners are unregistered. Since removing the
+  // event listener is more rare than the page visibility changing,
+  // Sensor::Suspend() is used to optimize this case for not doing extra work.
+
+  accelerometer_.Stop();
+  linear_acceleration_sensor_.Stop();
+  gyroscope_.Stop();
+}
+
+void DeviceMotionEventPump::FireEvent(TimerBase*) {
+  device::MotionData data;
+  // The device orientation spec states that interval should be in milliseconds.
+  // https://w3c.github.io/deviceorientation/spec-source-orientation.html#devicemotion
+  data.interval = kDefaultPumpDelayMicroseconds / 1000;
+
+  DCHECK(listener());
+
+  GetDataFromSharedMemory(&data);
+
+  if (ShouldFireEvent(data))
+    listener()->DidChangeDeviceMotion(data);
+}
+
+bool DeviceMotionEventPump::SensorsReadyOrErrored() const {
+  return accelerometer_.ReadyOrErrored() &&
+         linear_acceleration_sensor_.ReadyOrErrored() &&
+         gyroscope_.ReadyOrErrored();
+}
+
+void DeviceMotionEventPump::GetDataFromSharedMemory(device::MotionData* data) {
+  // "Active" here means that sensor has been initialized and is either ready
+  // or not available.
+  bool accelerometer_active = true;
+  bool linear_acceleration_sensor_active = true;
+  bool gyroscope_active = true;
+
+  if (accelerometer_.SensorReadingCouldBeRead() &&
+      (accelerometer_active = accelerometer_.reading.timestamp() != 0.0)) {
+    data->acceleration_including_gravity_x = accelerometer_.reading.accel.x;
+    data->acceleration_including_gravity_y = accelerometer_.reading.accel.y;
+    data->acceleration_including_gravity_z = accelerometer_.reading.accel.z;
+    data->has_acceleration_including_gravity_x =
+        !std::isnan(accelerometer_.reading.accel.x.value());
+    data->has_acceleration_including_gravity_y =
+        !std::isnan(accelerometer_.reading.accel.y.value());
+    data->has_acceleration_including_gravity_z =
+        !std::isnan(accelerometer_.reading.accel.z.value());
+  }
+
+  if (linear_acceleration_sensor_.SensorReadingCouldBeRead() &&
+      (linear_acceleration_sensor_active =
+           linear_acceleration_sensor_.reading.timestamp() != 0.0)) {
+    data->acceleration_x = linear_acceleration_sensor_.reading.accel.x;
+    data->acceleration_y = linear_acceleration_sensor_.reading.accel.y;
+    data->acceleration_z = linear_acceleration_sensor_.reading.accel.z;
+    data->has_acceleration_x =
+        !std::isnan(linear_acceleration_sensor_.reading.accel.x.value());
+    data->has_acceleration_y =
+        !std::isnan(linear_acceleration_sensor_.reading.accel.y.value());
+    data->has_acceleration_z =
+        !std::isnan(linear_acceleration_sensor_.reading.accel.z.value());
+  }
+
+  if (gyroscope_.SensorReadingCouldBeRead() &&
+      (gyroscope_active = gyroscope_.reading.timestamp() != 0.0)) {
+    data->rotation_rate_alpha = gfx::RadToDeg(gyroscope_.reading.gyro.x);
+    data->rotation_rate_beta = gfx::RadToDeg(gyroscope_.reading.gyro.y);
+    data->rotation_rate_gamma = gfx::RadToDeg(gyroscope_.reading.gyro.z);
+    data->has_rotation_rate_alpha =
+        !std::isnan(gyroscope_.reading.gyro.x.value());
+    data->has_rotation_rate_beta =
+        !std::isnan(gyroscope_.reading.gyro.y.value());
+    data->has_rotation_rate_gamma =
+        !std::isnan(gyroscope_.reading.gyro.z.value());
+  }
+
+  data->all_available_sensors_are_active = accelerometer_active &&
+                                           linear_acceleration_sensor_active &&
+                                           gyroscope_active;
+}
+
+bool DeviceMotionEventPump::ShouldFireEvent(
+    const device::MotionData& data) const {
+  return data.all_available_sensors_are_active;
+}
+
+}  // namespace blink
