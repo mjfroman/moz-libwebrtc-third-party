506fed68e7ae5900ba335f9551bcc1bc87e7f992
Guido Urdaneta <guidou@chromium.org>
2019-01-17 17:29:50 +0000

    Move content::MediaStreamSource to Blink.
    This is part of Onion Souping content/renderer/media/stream.
    
    This CL merely moves the type from content/renderer/media/stream
    to blink/public/platform/modules/mediastream.
    
    Follow-up CLs will take advantage of this move to start reducing layers.
    
    Bug: 921006
    Change-Id: Ibdc0df9aa0c9b8cae51dee56f9f50a80a72d989e
    
    TBR: haraken@chromium.org
    Change-Id: Ibdc0df9aa0c9b8cae51dee56f9f50a80a72d989e
    Reviewed-on: https://chromium-review.googlesource.com/c/1406681
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#623736}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 9700d2b7a0270cf9abc8b6ca9dcbdb2e092f71fc

---
+++ blink/renderer/platform/exported/platform_media_stream_source.cc
@@ -0,0 +1,65 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/public/platform/modules/mediastream/platform_media_stream_source.h"
+
+#include "base/logging.h"
+
+namespace blink {
+
+const char kMediaStreamSourceTab[] = "tab";
+const char kMediaStreamSourceScreen[] = "screen";
+const char kMediaStreamSourceDesktop[] = "desktop";
+const char kMediaStreamSourceSystem[] = "system";
+
+const char PlatformMediaStreamSource::kSourceId[] = "sourceId";
+
+PlatformMediaStreamSource::PlatformMediaStreamSource() {}
+
+PlatformMediaStreamSource::~PlatformMediaStreamSource() {
+  DCHECK(stop_callback_.is_null());
+}
+
+void PlatformMediaStreamSource::StopSource() {
+  DoStopSource();
+  FinalizeStopSource();
+}
+
+void PlatformMediaStreamSource::FinalizeStopSource() {
+  if (!stop_callback_.is_null())
+    base::ResetAndReturn(&stop_callback_).Run(Owner());
+  Owner().SetReadyState(blink::WebMediaStreamSource::kReadyStateEnded);
+}
+
+void PlatformMediaStreamSource::SetSourceMuted(bool is_muted) {
+  // Although this change is valid only if the ready state isn't already Ended,
+  // there's code further along (like in blink::MediaStreamTrack) which filters
+  // that out already.
+  Owner().SetReadyState(is_muted
+                            ? blink::WebMediaStreamSource::kReadyStateMuted
+                            : blink::WebMediaStreamSource::kReadyStateLive);
+}
+
+void PlatformMediaStreamSource::SetDevice(
+    const blink::MediaStreamDevice& device) {
+  device_ = device;
+}
+
+void PlatformMediaStreamSource::SetStopCallback(
+    const SourceStoppedCallback& stop_callback) {
+  DCHECK(stop_callback_.is_null());
+  stop_callback_ = stop_callback;
+}
+
+void PlatformMediaStreamSource::ResetSourceStoppedCallback() {
+  DCHECK(!stop_callback_.is_null());
+  stop_callback_.Reset();
+}
+
+void PlatformMediaStreamSource::ChangeSource(
+    const blink::MediaStreamDevice& new_device) {
+  DoChangeSource(new_device);
+}
+
+}  // namespace blink
