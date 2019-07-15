4918e8a74365911effa618f526d21684c8c4ed00
Yoav Weiss <yoavweiss@chromium.org>
2019-07-15 22:34:03 +0000

    Verify HTML was AppCache fetched before delaying subresource requests
    Currently we delay subresource requests until the document element is
    initialized, in order to be sure AppCache was initialized as well.
    That is entirely unnecessary for pages which don't use AppCache, which
    is most of them.
    
    This CL changes that behavior to only apply that delay in cases where
    the HTML was actually fetched from AppCache.
    
    Bug: 788757
    Change-Id: I2177c05ba7f3b59c43b70a5cf9c17a75c4bd6996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1676645
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#677526}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: ddfb8d2edcf3f1269d6cf7db2457932037fdd8ed

--- blink/web_tests/http/tests/loading/appcache-delay/link_header_preload_on_commit.php
+++ blink/web_tests/http/tests/loading/appcache-delay/link_header_preload_on_commit.php
@@ -28,10 +28,10 @@
       // Note: the link preloads can be preloaded before the normal resource
       // because they don't need to wait for the document element to be
       // available (ApplicationCache initialization time).
-      assert_greater_than(markup.startTime, nonMediaHeader.startTime);
-      assert_greater_than(mediaHeader.startTime, nonMediaHeader.startTime);
-      assert_greater_than(normalResource.startTime, mediaHeader.startTime);
-      assert_greater_than(normalResource.startTime, markup.startTime);
+      assert_true(markup.startTime >= nonMediaHeader.startTime, "Non media header image requested before markup");
+      assert_true(mediaHeader.startTime >= nonMediaHeader.startTime, "Non media header requested before media header");
+      assert_true(normalResource.startTime >= mediaHeader.startTime, "Media heaer requested before non-preload resource");
+      assert_true(markup.startTime >= normalResource.startTime, "Markup preload request after non-preload resource with same priority that's declared before it");
       t.done();
     }));
 </script>
